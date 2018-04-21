3b8b137d0b62fa36cfc3758e0dfd0269373c6989
Daya Kern <s4313512@uq.edu.au>
2018-04-21 15:22:14 +1000

    updated with different events

--- Coffee/src/main/GuiController.java
+++ Coffee/src/main/GuiController.java
@@ -4,26 +4,75 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
 
 public class GuiController implements Initializable {
 
 	@FXML
+	private AnchorPane searchResults;
+	@FXML
+	private AnchorPane loadingScreen;
+	@FXML
+	private AnchorPane friendResult;
+	@FXML
+	private AnchorPane home; 
+	@FXML
+	private AnchorPane homeSearch;
+	
+	@FXML
 	private Button photoBtn;
-
+	
+	@FXML
+	private TextField searchBar;
+	
+	@FXML
+	private ImageView searchImg;
 
 	/**
-	 * Ensures that you cannot download with a file chosen.
+	 * Fake reset
 	 */
 	@FXML
-	public void takeSneakyShot() {
-		photoBtn.setDisable(true);
+	private void returnHome() {
+		searchResults.setVisible(false);
+		friendResult.setVisible(false);
+		searchBar.clear();
 	}
 
-
+	/**
+	 * Launch photo taking spree
+	 */
+	@FXML
+	private void takeSneakyShot() {
+		//Take photo
+		//Do Machine Learning stuff (compare photo)
+		
+	}
+	 
+	/**
+	 * Find person by name 
+	 */
+	@FXML
+	private void searchByName() {
+		System.out.println(searchBar.getText()); 
+		
+		// Launch DB query
+//		loadingScreen.setVisible(true); //rip
+		searchResults.setVisible(true);
+	}
+	
+	@FXML
+	private void imageSelected() {
+		System.out.println("Selected: " + searchBar.getText());
+		friendResult.setVisible(true);
+	}
 
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
 		// Setting up file selection
 	}
+	
+	
 }
